feabefeb51fc316516c32d0b1f9ee8f874ceb6e0
Alejandro Munoz <alejandro.z.munoz@gmail.com>
2023-04-12 22:57:55 -0700

    adding tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import Button from "./Button";
 function App() {
-  return <div>App</div>
+  return (
+    <div>
+      <div>
+        <Button success primary rounded outline>Click Me!</Button>
+      </div>
+      <div>
+        <Button>Buy Now</Button>
+      </div>
+      <div>
+        <Button>See Now</Button>
+      </div>
+      <div>
+        <Button>Something</Button>
+      </div>
+      <div>
+        <Button>Hide Ads!</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
